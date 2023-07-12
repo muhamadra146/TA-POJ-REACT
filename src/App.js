847e7af86526567dc11f4d3ef74a98e6470131d3
muhamadra146 <muhamadra146@gmail.com>
2023-07-12 22:40:37 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
+        <Route path='/' Component={Discovery}/>
         <Route path='/discovery' Component={Discovery}/>
         <Route path='/destination' Component={Destination}/>
         <Route path='/package' Component={Package}/>
